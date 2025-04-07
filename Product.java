777816d58cb4d85f5959a05fcea2a10675566c34
szywat <szywat04@gmail.com>
2025-04-08 01:40:49 +0200

    +product class

---
+++ Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    public String code;
+    public String name;
+    public double price;
+    public double discountPrice;
+
+    public Product(String code, String name, double price, double discountPrice) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.discountPrice = discountPrice;
+    }
+}
